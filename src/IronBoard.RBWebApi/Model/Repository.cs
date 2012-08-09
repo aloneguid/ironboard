2fa8f0861f2bbdcec56faa2a1c28c5a039b2863b
SND\aloneguid_cp <SND\aloneguid_cp@4ef1bfeb-0a34-4d02-9305-240e5b07a168>
2012-08-09 13:58:02 +0000

    git-svn-id: https://ironboard.svn.codeplex.com/svn/trunk@94716 4ef1bfeb-0a34-4d02-9305-240e5b07a168

--- src/IronBoard.RBWebApi/Model/Repository.cs
+++ src/IronBoard.RBWebApi/Model/Repository.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace IronBoard.RBWebApi
+namespace IronBoard.RBWebApi.Model
 {
-   public class Class1
+   public class Repository
    {
    }
 }
---
+++ src/IronBoard.RBWebApi/RBClient.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using IronBoard.RBWebApi.Model;
+using RestSharp;
+
+namespace IronBoard.RBWebApi
+{
+   public class RBClient
+   {
+      private readonly Uri _webApiBaseUri;
+      private RestClient _client;
+
+      public RBClient(Uri webApiBaseUri)
+      {
+         if (webApiBaseUri == null) throw new ArgumentNullException("webApiBaseUri");
+         _webApiBaseUri = webApiBaseUri;
+         _client = new RestClient(webApiBaseUri.ToString());
+      }
+
+      public IEnumerable<Repository> GetRepositories()
+      {
+         //var request = new RestRequest()
+      }
+   }
+}
