1bd6f048c97f0b78a164c050111b85fa843bb682
SND\aloneguid_cp <SND\aloneguid_cp@4ef1bfeb-0a34-4d02-9305-240e5b07a168>
2012-08-09 15:14:00 +0000

    git-svn-id: https://ironboard.svn.codeplex.com/svn/trunk@94727 4ef1bfeb-0a34-4d02-9305-240e5b07a168

---
+++ src/IronBoard.RBWebApi/Application/ReviewBoardRc.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace IronBoard.RBWebApi.Application
+{
+   class ReviewBoardRc
+   {
+      private const string KeyReviewBoardUrl = "REVIEWBOARD_URL";
+      private const string KeyRepository = "REPOSITORY";
+
+      private readonly string _configPath;
+
+      public ReviewBoardRc(string rootFolder)
+      {
+         if (rootFolder == null) throw new ArgumentNullException("rootFolder");
+         if(!Directory.Exists(rootFolder)) throw new DirectoryNotFoundException("rootFolder");
+         _configPath = Path.Combine(rootFolder, ".reviewboardrc");
+         if (!File.Exists(_configPath)) throw new FileNotFoundException("config file not found", _configPath);
+      }
+
+      private void ReadConfiguration()
+      {
+         
+      }
+
+      public Uri Uri { get; private set; }
+
+      public string Repository { get; private set; } 
+   }
+}
--- src/IronBoard.RBWebApi/RBClient.cs
+++ src/IronBoard.RBWebApi/RBClient.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using IronBoard.RBWebApi.Application;
 using IronBoard.RBWebApi.Model;
 using RestSharp;
 
@@ -11,17 +12,22 @@ namespace IronBoard.RBWebApi
    {
       private readonly Uri _webApiBaseUri;
       private RestClient _client;
+      private ReviewBoardRc _config;
 
-      public RBClient(Uri webApiBaseUri)
+      public RBClient(Uri webApiBaseUri, string projectRootFolder)
       {
          if (webApiBaseUri == null) throw new ArgumentNullException("webApiBaseUri");
          _webApiBaseUri = webApiBaseUri;
-         _client = new RestClient(webApiBaseUri.ToString());
+         _client = new RestClient(new Uri(webApiBaseUri, "api").ToString());
+         _config = new ReviewBoardRc(projectRootFolder);
       }
 
       public IEnumerable<Repository> GetRepositories()
       {
-         //var request = new RestRequest()
+         var request = new RestRequest("repositories");
+         request.Method = Method.GET;
+
+         return null;
       }
    }
 }
