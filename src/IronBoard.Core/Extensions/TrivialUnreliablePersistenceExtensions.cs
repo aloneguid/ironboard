ff06c105683aef1348629e9f26efa8c960939ffc
SND\aloneguid_cp <SND\aloneguid_cp@4ef1bfeb-0a34-4d02-9305-240e5b07a168>
2012-08-24 14:02:18 +0000

    git-svn-id: https://ironboard.svn.codeplex.com/svn/trunk@95184 4ef1bfeb-0a34-4d02-9305-240e5b07a168

---
+++ src/IronBoard.Core/Extensions/TrivialUnreliablePersistenceExtensions.cs
@@ -0,0 +1,55 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+
+namespace System
+{
+   /// <summary>
+   /// These extensions must NEVER fail but be as much version agnostic as possible!!!
+   /// </summary>
+   public static class TrivialUnreliablePersistenceExtensions
+   {
+      public static T TrivialDeserialize<T>(this string s) where T : class
+      {
+         T r = default(T);
+
+         if (!string.IsNullOrEmpty(s))
+         {
+            try
+            {
+               var xs = new XmlSerializer(typeof(T));
+               byte[] buffer = Convert.FromBase64String(s);
+               using (var ms = new MemoryStream(buffer))
+               {
+                  r = xs.Deserialize(ms) as T;
+                  //todo: MSDN doesn't say which exceptions it may throw, gradually add the list
+               }
+            }
+            catch (FormatException)
+            {
+               //string is not in base64 format
+            }
+         }
+
+         return r;
+      }
+
+      /// <summary>
+      /// Use to serialize simple "xml-compatible" classes
+      /// </summary>
+      /// <param name="o"></param>
+      /// <returns></returns>
+      public static string TrivialSerialize(this object o)
+      {
+         if (o == null) return null;
+
+         //do not catch exceptions, incompatible classes must throw runtime errors
+
+         var xs = new XmlSerializer(o.GetType());
+         using (var ms = new MemoryStream())
+         {
+            xs.Serialize(ms, o);
+            return Convert.ToBase64String(ms.ToArray());
+         }
+      }
+   }
+}
---
+++ src/IronBoard.Core/Model/CoreSettings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IronBoard.Core.Model
+{
+   public class CoreSettings
+   {
+      public string UserName { get; set; }
+
+      public string Password { get; set; }
+
+      public int MaxRevisions { get; set; }
+
+      public string TargetUsers { get; set; }
+
+      public string TargetGroups { get; set; }
+   }
+}
