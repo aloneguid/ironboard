2a05831c982ff3cee9094edfec89594ee032a53a
SND\aloneguid_cp <SND\aloneguid_cp@4ef1bfeb-0a34-4d02-9305-240e5b07a168>
2012-10-31 22:28:08 +0000

    git-svn-id: https://ironboard.svn.codeplex.com/svn/trunk@96921 4ef1bfeb-0a34-4d02-9305-240e5b07a168

---
+++ src/IronBoard.Core/Wpf/ReviewDetailsView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace IronBoard.Core.Wpf
+{
+   /// <summary>
+   /// Interaction logic for ReviewDetailsView.xaml
+   /// </summary>
+   public partial class ReviewDetailsView : Window
+   {
+      public ReviewDetailsView()
+      {
+         InitializeComponent();
+      }
+   }
+}
