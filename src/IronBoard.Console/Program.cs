c968e237afe5069e341667ba735d81f47f4a9493
SND\aloneguid_cp <SND\aloneguid_cp@4ef1bfeb-0a34-4d02-9305-240e5b07a168>
2013-01-03 18:45:02 +0000

    git-svn-id: https://ironboard.svn.codeplex.com/svn/trunk@99078 4ef1bfeb-0a34-4d02-9305-240e5b07a168

---
+++ src/IronBoard.Console/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using IronBoard.Core;
+
+namespace IronBoard.Console
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+      }
+   }
+}
